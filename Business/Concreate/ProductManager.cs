5e664b75d8961df69321191e69f9e7c40a8dafec
caferyavuz <caferyavuz134@gmail.com>
2021-04-15 18:31:57 +0300

    Autofac updated

--- Business/Concreate/ProductManager.cs
+++ Business/Concreate/ProductManager.cs
@@ -1,10 +1,14 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concreate.InMemory;
 using Entities.Concreate;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,14 +24,10 @@ namespace Business.Concreate
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
 
-            if (product.ProductName.Length<2)
-            {
-                //magic strings
-                return new ErrorResult(Messages.ProductNameInValid);
-            }
             _productDal.Add(product);
 
             return new SuccessResult(Messages.ProductAdded);
